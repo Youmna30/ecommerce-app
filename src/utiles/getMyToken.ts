f04a71264f86642dbcf2c1d26232ad241bf4db17
Youmna30 <youmnaashraf968662@gmail.com>
2026-04-16 03:51:35 +0100

    change cookies name on server

--- src/utiles/getMyToken.ts
+++ src/utiles/getMyToken.ts
@@ -3,7 +3,7 @@ import { cookies } from "next/headers";
 
 export default async function getMyToken(){
     const myCookies = await cookies()
-    const tokenFromCookies =  myCookies.get("next-auth.session-token")?.value;
+    const tokenFromCookies =  myCookies.get("__Secure-next-auth.session-token")?.value;
     const decodedToken = await decode({token:tokenFromCookies, secret:process.env.BETTER_AUTH_SECRET!});
     return decodedToken?.realBackendToken;
 }
