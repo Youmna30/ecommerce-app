9c98e1b4bf997c5849c09283a68a2fd9f68bf6d5
Youmna30 <youmnaashraf968662@gmail.com>
2026-04-16 03:59:31 +0100

    cache categories

--- src/services/category.service.ts
+++ src/services/category.service.ts
@@ -3,7 +3,9 @@ import { CategoryType } from "@/types/category.type";
 export async function getAllCategories() : Promise< CategoryType[] | null >{
     
     try {
-        const data = await fetch(`${process.env.BACK_END_API}/v1/categories`)
+        const data = await fetch(`${process.env.BACK_END_API}/v1/categories`,{
+            cache:"force-cache"
+        })
         const categories = await data.json();
         return categories.data
     } catch (error) {
--- src/utiles/getMyToken.ts
+++ src/utiles/getMyToken.ts
@@ -3,6 +3,8 @@ import { cookies } from "next/headers";
 
 export default async function getMyToken(){
     const myCookies = await cookies()
+    // next-auth.session-token locaal
+    // __Secure-next-auth.session-token // production
     const tokenFromCookies =  myCookies.get("__Secure-next-auth.session-token")?.value;
     const decodedToken = await decode({token:tokenFromCookies, secret:process.env.BETTER_AUTH_SECRET!});
     return decodedToken?.realBackendToken;
